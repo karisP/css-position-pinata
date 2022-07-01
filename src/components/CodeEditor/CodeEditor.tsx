2ef829f633d3d4d0d87579b1e4aa2df5a5a6ebfd
Karissa Phillips <phillips.karissa00@gmail.com>
2022-07-01 15:53:53 -0400

    basic ui for code editor

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -1,5 +1,30 @@
+import styles from "./CodeEditor.module.css";
+
 const CodeEditor = () => {
-  return <div>Code Editor</div>;
+  return (
+    <div className={styles.container}>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+      </ul>
+      <div className={styles.syntax}>
+        <pre>
+          #parent &#123; <br /> position: relative;
+        </pre>
+        <textarea></textarea>
+        <pre>&#125;</pre>
+      </div>
+      <button>Submit</button>
+    </div>
+  );
 };
 
 export default CodeEditor;
