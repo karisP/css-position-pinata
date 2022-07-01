8c6e595b588d3d867639148c18b19e3ed1ce5364
Karissa Phillips <phillips.karissa00@gmail.com>
2022-07-01 15:15:23 -0400

    add some default Instructions

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -1,5 +1,35 @@
+import styles from "./Instructions.module.css";
+
 const Instructions = () => {
-  return <div>Instructions</div>;
+  return (
+    <div className={styles.container}>
+      <h1>CSS Position Pinata</h1>
+      <p>
+        Welcome to the Pinata Party! This game was created to help you
+        understand and practice using the css position properties.
+      </p>
+      <ul>
+        <li>
+          relative: relates to where it normally is. Most elements are
+          positioned in the top left corner.
+        </li>
+        <li>
+          absolute: is positioned relative to the nearest positioned ancestor or
+          the document body
+        </li>
+        <li>
+          fixed: positioned relative to the viewport, it will always stay in the
+          same place
+        </li>
+        <li>
+          sticky: positioned based on the user's scroll position. If you scroll,
+          it will stick with you.
+        </li>
+        <li>static: elements are positioned static by default</li>
+      </ul>
+      <p>Some directions about the next steps.</p>
+    </div>
+  );
 };
 
 export default Instructions;
