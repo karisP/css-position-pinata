b72c10514077486b60e8b849f18b75c4d7f3307e
Karissa Phillips <phillips.karissa00@gmail.com>
2022-07-13 17:48:32 -0400

    added data files for instructions and code bites

---
+++ src/data/codeSnippets.ts
@@ -0,0 +1,39 @@
+type ClassName = {
+  class: string;
+  properties?: string[];
+  input: boolean;
+};
+
+type CodeSnippets = {
+  classNames: ClassName[];
+  properties?: string[];
+  expected: string;
+};
+
+export const codeSnippets: CodeSnippets[] = [
+  {
+    classNames: [
+      {
+        class: 'canvas',
+        properties: [],
+        input: true
+      }
+    ],
+    expected: 'position: relative;'
+  },
+  {
+    classNames: [
+      {
+        class: 'canvas',
+        properties: ['position: relative'],
+        input: false
+      },
+      {
+        class: 'grass',
+        properties: [],
+        input: true
+      }
+    ],
+    expected: 'position: fixed;'
+  }
+];
---
+++ src/data/instructions.tsx
@@ -0,0 +1,15 @@
+type Instructions = {
+  text: string;
+  action: string | null;
+};
+
+export const instructions: Instructions[] = [
+  {
+    text: 'You have been given a canvas to build the scenery for the party. The canvas is currently position static by default. Start by setting the canvas to position relative.',
+    action: 'Next'
+  },
+  {
+    text: 'Now we need to set the grass to the bottom of the screen using position fixed',
+    action: 'Next'
+  }
+];
